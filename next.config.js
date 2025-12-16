5ca582fee7d6cd3910804f3bbbbc38242e9be3bf
Ricco de Carvalho <riccodecarvalho@gmail.com>
2025-12-16 00:49:16 -0300

    fix: Configura webpack externals para ffmpeg/ffprobe
    - Adiciona serverComponentsExternalPackages no next.config.js
    - Configura webpack externals para evitar bundling do README.md
    - Resolve erro 500 no /jobs/new

--- next.config.js
+++ next.config.js
@@ -1,8 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Configuração para suportar better-sqlite3 no servidor
+    // Configuração para suportar pacotes nativos/server no servidor
     experimental: {
-        serverComponentsExternalPackages: ['better-sqlite3'],
+        serverComponentsExternalPackages: [
+            'better-sqlite3',
+            'fluent-ffmpeg',
+            '@ffmpeg-installer/ffmpeg',
+            '@ffprobe-installer/ffprobe',
+            'adm-zip',
+        ],
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            // Marcar pacotes nativos como externals
+            config.externals = config.externals || [];
+            config.externals.push({
+                '@ffprobe-installer/ffprobe': 'commonjs @ffprobe-installer/ffprobe',
+                '@ffmpeg-installer/ffmpeg': 'commonjs @ffmpeg-installer/ffmpeg',
+                'fluent-ffmpeg': 'commonjs fluent-ffmpeg',
+            });
+        }
+        return config;
     },
 }
 
